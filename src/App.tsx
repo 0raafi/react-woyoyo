fee677c6cbd8e0f4ccb0ecab2901fa2e206886b8
Raafi Muhammad <rafmhmd06@gmail.com>
2021-11-18 15:32:03 +0700

    + Eslintrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { renderRoutes } from 'react-router-config';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import routes from './routes';
 
 export default function App() {
-  return                      <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>;
+  return (
+    <BrowserRouter>
+      {renderRoutes(routes) }
+    </BrowserRouter>
+  );
 }
